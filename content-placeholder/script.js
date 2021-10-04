165d330facc2cc13917824eb965e11e220dc68c2
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-04 16:30:33 +0100

    some changes to css

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -12,10 +12,10 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 setTimeout(getData, 1500)
 
 function getData() {
-  header.innerHTML = '<img src="https://images.unsplash.com/photo-1633285718659-0f28d8e561be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1372&q=80" alt=""/>'
+  header.innerHTML = '<img src="https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80" alt=""/>'
   title.innerHTML ='Some random text'
   excerpt.innerHTML ='Some other text'
-  profile_img.innerHTML ='<img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" alt=""/>'
+  profile_img.innerHTML ='<img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80" alt=""/>'
   name.innerHTML = 'John Doe'
   date.innerHTML = 'Oct 04 2021'
 
