0131da5ca2adab5c5de242f8d0fbc063031dff9d
Kateryna Ueno <katerynaueno@gmail.com>
2021-10-16 12:02:06 +0100

    Almost finished

--- countdown/setup/app.js
+++ countdown/setup/app.js
@@ -41,3 +41,36 @@ month = months[month];
 let date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 giveaway.textContent = `giveaway ends on ${date} ${month}, ${weekday} ${year} at ${hours}:${minutes}am`;
+
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const time = futureTime - today;
+
+  // 1sec = 1000ms
+  // 1m = 60sec
+  // 1h = 60min
+  // 1day = 24h
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = time / oneDay;
+  days = Math.floor(days);
+
+  let hours = Math.floor((time % oneDay) / oneHour);
+  // console.log(hours);
+
+  let minutes = Math.floor((time % oneHour) / oneMinute);
+
+  let seconds = Math.floor((time % oneMinute) / 1000);
+  console.log(seconds);
+
+}
+
+
+getRemainingTime();
